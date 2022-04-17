f8b423c19390eb675a123193314470d4431ed586
Rajesh Yadav <rajeshyadav682@hotmail.com>
2022-04-17 18:36:28 +0530

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAppWeb.Data;
+using MyAppWeb.Models;
+
+namespace MyAppWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDBContext _context;
+
+        public CategoryController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+       
+        public IActionResult Index()
+        {
+            IEnumerable<Category> Categories = _context.categories;
+            return View(Categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id)
+        {
+           var category = _context.categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAppWeb.Models;
+
+namespace MyAppWeb.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> categories { get; set; }
+    }
+}
